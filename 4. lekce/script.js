e382f1a6e6f19d61dce37520e6f4fba2b80c8d26
Ema136 <Ema.polanska@post.cz>
2026-03-13 10:25:30 +0100

    13.3.

--- 4. lekce/script.js
+++ 4. lekce/script.js
@@ -3,25 +3,29 @@ let isAdmin = true;
 
 if (isLoggednIn) {
 
-    console.log("Vítej na stránce")
-}
+    console.log("Vítej na stránce");
+
 
 if (isAdmin){
 
     console.lop("přístup do adminu povolen.")
-} else {
+ }else {
 console.log("Jsi přihlášen jako uživatel")
-}
- else {
+}  
+{
     console.log("Nejsi přihlášen")
 }
-
+}
 let age = 31;
 let hasDriverLicense = true;
 
 if (age>= 18 && hasDriverLicense){
 
 console.log("Nemůžeš řídit!")
-} else{
+} else {
     console.log("Nemůžeš řídit!")
 }
+let isHoliday = false;
+let isWeekend = true;
+
+
