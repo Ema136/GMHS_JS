d977a0d781a846fce8a5c7bc51fc0e3daa85b87b
Ema136 <Ema.polanska@post.cz>
2026-03-20 10:36:34 +0100

    20.3.2026

--- 5. Lekce/script.js
+++ 5. Lekce/script.js
@@ -8,7 +8,7 @@ if (age >3 && age <= 12)
 if (age > 13 && age <= 17)
 console.log("Vstupné = 7$");
 if(age > 18 && age <=64)
-console.log("Vatupné = 10$");
+console.log("Vstupné = 10$");
 if(age >=65)
 console.log("Vstupné = 6$")
 }
